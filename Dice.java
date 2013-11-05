75b630126481f55cce6521a888671aa96aded2c8
nnguyen14640 <nnguyen14640@yahoo.com>
2013-11-04 23:39:43 -0800

    Changed and edited the Dice and GuessANumberDiceGame programs.

--- Dice.java
+++ Dice.java
@@ -5,24 +5,58 @@ import java.util.Random;
 
 public class Dice 
 {
-	private int die_value;
 	Random randomizer = new Random();
+	private int average;
+	private int n_bounces;
+	private int n_dices;
 	
-	// Constructs and initializes the die value
+	// Constructor to initialize dices and bounces
+	public Dice(int dices, int bounces)
+	{
+		n_dices = dices;
+		n_bounces = bounces;
+	}
 	public Dice()
 	{
-		die_value = randomizer.nextInt(6) + 1;
+		n_bounces = 1;
+		n_dices = 1;
 	}
 	
-	// Sets the value of the die
-	public void Throw(int value)
+	// Static method to throw a dice only once
+	public static int OneThrow()
 	{
-		die_value = value;
+		return (int) (Math.random() * 6 + 1);
+	}
+	// Throw method to throw a certain number of dices for each trial
+	public int Throw(int dices, int bounces)
+	{
+		n_dices = dices;
+		n_bounces = bounces;
+		
+		int sums = 0;
+		int value = 0;
+		for (int i = 0; i < bounces; i++)
+		{
+			System.out.println("\nTrial " + (i + 1));
+			int roll_value = 0;
+			for (int j = 0; j < n_dices; j++)
+			{
+				System.out.println("\nDice " + (j + 1));
+				value = randomizer.nextInt(6) + 1;
+				roll_value += value;
+				sums += value;
+				System.out.println("Rolled a " + value);
+			}
+			System.out.println("\nRolled value: " + roll_value);
+			System.out.println("Current value: " + sums);
+		}
+		average = sums / n_bounces;
+		return average;
 	}
 	
-	// Returns the value of the die
+	// Return the average value of numbers in Throw()
 	public int Value()
 	{
-		return die_value;
+		return average;
 	}
 }
--- GuessANumberDiceGame.java
+++ GuessANumberDiceGame.java
@@ -1,17 +1,87 @@
 // Nam Nguyen
 // Cmpr 112
 
+import java.util.Scanner;
+
 public class GuessANumberDiceGame 
 {
 	public static void main(String[] args)
 	{
-		// Construct and use the Dice class in this class
-		Dice die = new Dice();
-		int cpu_value;
+		Scanner input = new Scanner(System.in);
+		Dice die = new Dice(); // Initialize the Dice class
+		String[] statuses = {"Won", "Lost"};
+		String status = null;
+		int gamemode;
+		int dices = 1;
+		int trials = 0;
+		int rolled_number = 0;
+		int guess = 0;
 		
-		// Retrieving the value from the Dice class
-		cpu_value = die.Value();
+		System.out.println("Welcome to the Dice Game.");
+		System.out.println("Choose your game mode.");
 		
-		System.out.println("CPU Number: " + cpu_value);
+		do
+		{
+			System.out.print("Enter 1 for single-roll play or 2 for normal play. ");
+			gamemode = input.nextInt();
+			
+			if (gamemode == 1) // Prompts user to enter a number in a one-time dice roll
+			{
+				System.out.println("OK then. Let the games begin.");
+				rolled_number = Dice.OneThrow(); // Calls OneThrow method from Dice class
+				System.out.print("A 6-sided die is thrown. Do you know the number? ");
+				guess = input.nextInt();
+				System.out.println("The number rolled was a " + rolled_number + ".");
+				
+				if (guess == rolled_number)
+				{
+					System.out.println("Congratulations! That was the correct number.");
+					status = statuses[0];
+				}
+				else
+				{
+					System.out.println("Sorry! That was the wrong guess.");
+					status = statuses[1];
+				}	
+			}
+			else if (gamemode == 2) // Prompts user to enter dices to roll and trials to execute
+			{
+				System.out.print("How many dices do you want to roll? ");
+				dices = input.nextInt();
+				
+				System.out.print("How many trials do you want to execute? ");
+				trials = input.nextInt();
+				
+				System.out.println("OK then. Let us roll the dices for you.");
+			}
+			else // For not entering 1 or 2
+			{
+				System.out.println("Invalid entry.");
+			}
+		}
+		while(gamemode != 1 && gamemode != 2);
+		
+		System.out.println("\nAll trials are executed. Computing game history...");
+		System.out.println("\nGame History");
+		System.out.println("------------");
+		
+		if(gamemode == 1)
+		{
+			System.out.println("Actual guess: " + rolled_number);
+			System.out.println("Your guess: " + guess);
+			System.out.println("Result: " + status);
+		}
+		else if (gamemode == 2)
+		{
+			// Calls Throw() method of Dice class to roll the dices for the trials
+			die.Throw(dices, trials); 
+			
+			// Calls Value() method of Dice class to retrieve the average value of the trials
+			System.out.println("\nAverage: " + die.Value());
+			System.out.println("Dices rolled: " + dices);
+			System.out.println("Trials played: " + trials);
+		}
+		
+		input.close();	
 	}
 }
